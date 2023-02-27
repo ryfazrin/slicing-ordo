faea49b0e54528475ee6de33ad55a51c5fed6aa5
ryfazrin <riansera515@gmail.com>
2023-02-27 11:59:30 +0700

    init project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="/css/normalize.css" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Nunito';
+        }
+    </style>
+</head>
+
+<body class="antialiased">
+    <header>
+        <nav>
+            <a class="navbar-brand" href="#">
+                <img src="assets/logo.png" alt="logo">
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Event</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Catalog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About US</a>
+                </li>
+                <li class="nav-item">
+                    <a class="" href="#">ID</a> | <a class="" href="#">EN</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h1>About US</h1>
+        </section>
+        <section>
+            <h1>Service</h1>
+        </section>
+        <section>
+            <h1>Our Service</h1>
+        </section>
+        <section>
+            <h1>Holes & Spaces</h1>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
